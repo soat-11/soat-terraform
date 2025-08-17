ceaa87f01e7ade7652137c3203e70a377a512b3c
Arão <arao.lima@caveo.com.br>
2025-08-17 17:36:18 -0300

    feature: Update EKS node group configuration with subnet IDs and instance types

--- k8s-node/main.tf
+++ k8s-node/main.tf
@@ -2,7 +2,10 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = var.cluster_name
   node_group_name = "${var.project}-node-group"
   node_role_arn   = var.eks_role_arn
-  subnet_ids      = var.subnet_ids
+
+  subnet_ids     = var.subnet_ids
+  instance_types = ["t3.micro"]
+
 
   scaling_config {
     desired_size = 1
@@ -12,5 +15,10 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   update_config {
     max_unavailable = 1
+  }
+
+  tags = {
+    Name = "${var.project}-node"
+
   }
 }
