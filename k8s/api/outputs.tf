a55d96837403856af8578bdae11fdcb49a0169c7
Arão <arao.lima@caveo.com.br>
2025-09-07 21:56:48 -0300

    feature: Remove deprecated Horizontal Pod Autoscaler, outputs, and app_name variable from Kubernetes API configuration

--- k8s/api/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "api_image_full" {
-  value = kubernetes_deployment.kubernetes_deployment.spec[0].template[0].spec[0].container[0].image
-}
--- k8s/api/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "app_name" {
-  description = "API name"
-  type        = string
-}
