0852f1e80b0c12ff029d2e51b1210e12ecf24191
Arão <arao.lima@caveo.com.br>
2025-09-14 16:05:22 -0300

    feature: Refactor aws_route_table_association to use a for loop for subnet_id mapping

--- route-table/route-table.tf
+++ route-table/route-table.tf
@@ -8,7 +8,12 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "route_table_association" {
-  for_each       = toset(var.subnet_ids)
+  for_each = {
+    for idx, subnet_id in var.subnet_ids :
+    idx => subnet_id
+  }
+
   subnet_id      = each.value
   route_table_id = aws_route_table.route_table.id
 }
+
